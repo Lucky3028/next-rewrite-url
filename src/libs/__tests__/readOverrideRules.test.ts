2f7221cd1d0c8bc73872a87e0660d90436b65256
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-03-19 17:55:38 +0900

    test: use mockResolvedValue

--- src/libs/__tests__/readOverrideRules.test.ts
+++ src/libs/__tests__/readOverrideRules.test.ts
@@ -16,9 +16,7 @@ describe('readJsonAsOverrideRules', () => {
     ['multi key value pairs', { key: 'value', key2: 'value2' }],
     ['nested key value pair', { nested: { key: 'value' } }],
   ])('should return values with valid Json: %s', async (_, object) => {
-    vi.mocked(fs.readFile).mockImplementation(async () =>
-      JSON.stringify(object),
-    );
+    vi.mocked(fs.readFile).mockResolvedValue(JSON.stringify(object));
 
     await expect(readJsonAsOverrideRules('file')).resolves.toStrictEqual(
       object,
@@ -29,9 +27,7 @@ describe('readJsonAsOverrideRules', () => {
     ['value is boolean', { true: false }],
     ['value is double', { true: 0.1, key2: 'value2' }],
   ])('should throw a ZodError with invalid Json: %s', async (_, object) => {
-    vi.mocked(fs.readFile).mockImplementation(async () =>
-      JSON.stringify(object),
-    );
+    vi.mocked(fs.readFile).mockResolvedValue(JSON.stringify(object));
 
     await expect(readJsonAsOverrideRules('file')).rejects.toBeInstanceOf(
       ZodError,
