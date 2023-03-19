67344d6bc366278f29bcbde52a725b9e3ed45c28
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-03-19 13:04:18 +0900

    ci: run test only

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,7 +2,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  coverageReporters: [['text', { file: 'result-jest.txt' }]],
   // パスエイリアスの設定
   moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
   extensionsToTreatAsEsm: ['.ts'],
