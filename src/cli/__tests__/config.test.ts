596d0c73c5746671c8c46adb388e102176ee5c7c
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-03-19 21:59:54 +0900

    test: add error test for config reader

--- src/cli/__tests__/config.test.ts
+++ src/cli/__tests__/config.test.ts
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import fs from 'fs/promises';
+import { ZodError } from 'zod';
 
 import { readConfig } from '../config.js';
 
@@ -24,9 +26,84 @@ describe('readConfig', () => {
     vi.resetAllMocks();
   });
 
-  it('should return values with valid Json', async () => {
+  it('should return values with valid json', async () => {
     vi.mocked(fs.readFile).mockResolvedValue(JSON.stringify(filledConfig));
 
     await expect(readConfig('file')).resolves.toStrictEqual(filledConfig);
+  });
+
+  it.each([
+    ['no input field', { input: undefined }],
+    ['no outputs field', { outputs: undefined }],
+    [
+      'no ts field in outputs',
+      { outputs: { ...filledConfig.outputs, ts: undefined } },
+    ],
+    [
+      'no json field in outputs',
+      { outputs: { ...filledConfig.outputs, json: undefined } },
+    ],
+    ['empty string input', { input: '' }],
+  ])(`should throw an error: %s`, async (_, config) => {
+    vi.mocked(fs.readFile).mockResolvedValue(
+      JSON.stringify({ ...filledConfig, ...config }),
+    );
+
+    await expect(readConfig('file')).rejects.toBeInstanceOf(ZodError);
+  });
+
+  test('should throw an error when given unexpected extension output path as TS', async () => {
+    const { output: _, ...rest } = filledConfig.outputs.ts;
+    vi.mocked(fs.readFile).mockResolvedValue(
+      JSON.stringify({
+        ...filledConfig,
+        outputs: { ...filledConfig.outputs, ts: { ...rest, output: 'aiueo' } },
+      }),
+    );
+
+    await expect(readConfig('file')).rejects.toBeInstanceOf(ZodError);
+  });
+
+  test('should throw an error when given unexpected extension output path as json', async () => {
+    const { output: _, ...rest } = filledConfig.outputs.ts;
+    vi.mocked(fs.readFile).mockResolvedValue(
+      JSON.stringify({
+        ...filledConfig,
+        outputs: {
+          ...filledConfig.outputs,
+          json: { ...rest, output: 'aiueo' },
+        },
+      }),
+    );
+
+    await expect(readConfig('file')).rejects.toBeInstanceOf(ZodError);
+  });
+
+  test('should return config with default output TS file path when no given', async () => {
+    const { output: _, ...rest } = filledConfig.outputs.ts;
+    vi.mocked(fs.readFile).mockResolvedValue(
+      JSON.stringify({
+        ...filledConfig,
+        outputs: { ...filledConfig.outputs, ts: { ...rest } },
+      }),
+    );
+
+    await expect(
+      readConfig('file').then((s) => s.outputs.ts.output),
+    ).resolves.toContain('.ts');
+  });
+
+  test('should return config with default output json file path when no given', async () => {
+    const { output: _, ...rest } = filledConfig.outputs.json;
+    vi.mocked(fs.readFile).mockResolvedValue(
+      JSON.stringify({
+        ...filledConfig,
+        outputs: { ...filledConfig.outputs, json: { ...rest } },
+      }),
+    );
+
+    await expect(
+      readConfig('file').then((s) => s.outputs.json.output),
+    ).resolves.toContain('.json');
   });
 });
