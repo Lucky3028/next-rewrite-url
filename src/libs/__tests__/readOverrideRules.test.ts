d9ab45a2dc4f991d7b45d1013987e68c204feb2e
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-03-19 21:58:12 +0900

    test: fix string case

--- src/libs/__tests__/readOverrideRules.test.ts
+++ src/libs/__tests__/readOverrideRules.test.ts
@@ -15,7 +15,7 @@ describe('readJsonAsOverrideRules', () => {
     ['single key value pair', { key: 'value' }],
     ['multi key value pairs', { key: 'value', key2: 'value2' }],
     ['nested key value pair', { nested: { key: 'value' } }],
-  ])('should return values with valid Json: %s', async (_, object) => {
+  ])('should return values with valid json: %s', async (_, object) => {
     vi.mocked(fs.readFile).mockResolvedValue(JSON.stringify(object));
 
     await expect(readJsonAsOverrideRules('file')).resolves.toStrictEqual(
@@ -26,7 +26,7 @@ describe('readJsonAsOverrideRules', () => {
   it.each([
     ['value is boolean', { true: false }],
     ['value is double', { true: 0.1, key2: 'value2' }],
-  ])('should throw a ZodError with invalid Json: %s', async (_, object) => {
+  ])('should throw a ZodError with invalid json: %s', async (_, object) => {
     vi.mocked(fs.readFile).mockResolvedValue(JSON.stringify(object));
 
     await expect(readJsonAsOverrideRules('file')).rejects.toBeInstanceOf(
