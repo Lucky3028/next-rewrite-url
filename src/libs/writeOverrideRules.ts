d2b3fb10afd614a608b3e5156a511ec32c7aa92e
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-03-19 12:49:07 +0900

    feat: add writers

---
+++ src/libs/writeOverrideRules.ts
@@ -0,0 +1,21 @@
+import { writeFile } from 'fs/promises';
+
+const options: Parameters<typeof writeFile>['2'] = { encoding: 'utf-8' };
+
+export const writeOverrideRulesAsTs = async (
+  filePath: string,
+  data: unknown,
+  isNamedExport: boolean,
+) => {
+  const json = JSON.stringify(data);
+  const namedExportContents = `export const overrideRules = ${json};`;
+  const defaultExportContents = `const overrideRules = ${json};\n\nexport default overrideRules;`;
+  const contents = isNamedExport ? namedExportContents : defaultExportContents;
+
+  return writeFile(filePath, contents, options);
+};
+
+export const writeOverrideRulesAsJson = async (
+  filePath: string,
+  data: unknown,
+) => writeFile(filePath, `${JSON.stringify(data)}`, options);
