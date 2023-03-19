f815012685df4a2729df1ae157cbee702154a422
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-03-19 17:50:22 +0900

    test: use spyOn and mocked

--- src/libs/__tests__/writeOverrideRules.test.ts
+++ src/libs/__tests__/writeOverrideRules.test.ts
@@ -1,19 +1,15 @@
+import fs from 'fs/promises';
+
 import {
   writeOverrideRulesAsJson,
   writeOverrideRulesAsTs,
 } from '../writeOverrideRules.js';
 
-afterEach(() => {
-  vi.restoreAllMocks();
-});
+vi.spyOn(fs, 'writeFile');
+vi.mocked(fs.writeFile).mockResolvedValue(undefined);
 
-beforeAll(() => {
-  vi.mock('node:fs/promises', async () => ({
-    ...(await vi.importActual<typeof import('node:fs/promises')>(
-      'node:fs/promises',
-    )),
-    writeFile: vi.fn().mockReturnValue(() => undefined),
-  }));
+beforeEach(() => {
+  vi.resetAllMocks();
 });
 
 const WRITE_FILE_OPTION = { encoding: 'utf-8' };
@@ -21,11 +17,9 @@ const OUTPUT_FILE_PATH = 'file/path';
 const OUTPUT_DATA = { data: 'is-here!' };
 
 test('writeOverrideRulesAsJson should call write function', async () => {
-  const { writeFile } = await import('node:fs/promises');
-
   await writeOverrideRulesAsJson(OUTPUT_FILE_PATH, OUTPUT_DATA);
 
-  expect(writeFile).toHaveBeenNthCalledWith(
+  expect(fs.writeFile).toHaveBeenNthCalledWith(
     1,
     OUTPUT_FILE_PATH,
     JSON.stringify(OUTPUT_DATA),
@@ -33,36 +27,30 @@ test('writeOverrideRulesAsJson should call write function', async () => {
   );
 });
 
-describe('writeOverrideRulesAsTs', () => {
-  const dataStr = JSON.stringify(OUTPUT_DATA);
+const dataStr = JSON.stringify(OUTPUT_DATA);
 
-  test('should call write function with default export', async () => {
-    const { writeFile } = await import('node:fs/promises');
+test('writeOverrideRulesAsTs should call write function with default export', async () => {
+  await writeOverrideRulesAsTs(OUTPUT_FILE_PATH, OUTPUT_DATA, false);
 
-    await writeOverrideRulesAsTs(OUTPUT_FILE_PATH, OUTPUT_DATA, false);
+  const expectedData = `const overrideRules = ${dataStr};\n\nexport default overrideRules;`;
 
-    const expectedData = `const overrideRules = ${dataStr};\n\nexport default overrideRules;`;
+  expect(fs.writeFile).toHaveBeenNthCalledWith(
+    1,
+    OUTPUT_FILE_PATH,
+    expectedData,
+    WRITE_FILE_OPTION,
+  );
+});
 
-    expect(writeFile).toHaveBeenNthCalledWith(
-      1,
-      OUTPUT_FILE_PATH,
-      expectedData,
-      WRITE_FILE_OPTION,
-    );
-  });
+test('writeOverrideRulesAsTs should call write function with named export', async () => {
+  await writeOverrideRulesAsTs(OUTPUT_FILE_PATH, OUTPUT_DATA, true);
 
-  test('should call write function with named export', async () => {
-    const { writeFile } = await import('node:fs/promises');
+  const expectedData = `export const overrideRules = ${dataStr};`;
 
-    await writeOverrideRulesAsTs(OUTPUT_FILE_PATH, OUTPUT_DATA, true);
-
-    const expectedData = `export const overrideRules = ${dataStr};`;
-
-    expect(writeFile).toHaveBeenNthCalledWith(
-      1,
-      OUTPUT_FILE_PATH,
-      expectedData,
-      WRITE_FILE_OPTION,
-    );
-  });
+  expect(fs.writeFile).toHaveBeenNthCalledWith(
+    1,
+    OUTPUT_FILE_PATH,
+    expectedData,
+    WRITE_FILE_OPTION,
+  );
 });
