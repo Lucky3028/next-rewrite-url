c5c91b8932ea7f412ff9159ad7008d65730b3bb7
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-05-07 23:56:40 +0900

    fix: fix error if no folder

--- src/lib/writeRewriteRules.ts
+++ src/lib/writeRewriteRules.ts
@@ -4,6 +4,19 @@ import { RewriteRules } from './index.js';
 
 const options: Parameters<typeof fs.writeFile>['2'] = { encoding: 'utf-8' };
 
+const createFile = async (filePath: string, contents: string) => {
+  fs.writeFile(filePath, contents, options).catch(async (e: Error) => {
+    if (e.name === 'ENOENT') {
+      const dir = filePath.substring(0, filePath.lastIndexOf('/'));
+      await fs
+        .mkdir(dir, { recursive: true })
+        .then(async () => createFile(filePath, contents));
+    } else {
+      throw e;
+    }
+  });
+};
+
 export const writeRewriteRulesAsTs = async (
   filePath: string,
   rules: RewriteRules,
@@ -14,10 +27,10 @@ export const writeRewriteRulesAsTs = async (
   const defaultExportContents = `const rewriteRules = ${json};\n\nexport default rewriteRules;`;
   const contents = isNamedExport ? namedExportContents : defaultExportContents;
 
-  return fs.writeFile(filePath, contents, options);
+  return createFile(filePath, contents);
 };
 
 export const writeRewriteRulesAsJson = async (
   filePath: string,
   rules: RewriteRules,
-) => fs.writeFile(filePath, JSON.stringify(Object.values(rules)), options);
+) => createFile(filePath, JSON.stringify(Object.values(rules)));
